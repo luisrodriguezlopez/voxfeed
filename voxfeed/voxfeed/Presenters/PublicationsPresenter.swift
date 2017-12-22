efa5cce7a45500b676e25cbbe2864d7b925f80ee
Luis Rodriguez <lrodriguezpc@corporativosade.com.mx>
2017-12-22 17:30:29 -0600

    rx images pendiente

--- voxfeed/voxfeed/Presenters/PublicationsPresenter.swift
+++ voxfeed/voxfeed/Presenters/PublicationsPresenter.swift
@@ -33,18 +33,25 @@ class PublicationsPresenter : PublicationsProtocol{
     }
     
     func retrivePublications() {
+        var observableImages : Observable<[NSDictionary]>!
         let observablePublications = self.rx_getPublications()
         observablePublications.subscribe(onNext: { (response) in
-            
-        print(response)
-        }, onError: { (error) in
-            print(error)
-        }, onCompleted: {
-            print("complet")
-        }) {
-    
-        }
+            print(response)
         
+            }, onError: { (error) in
+                print(error)
+            }, onCompleted: {
+//             self.rx_retriveImages(promotedMessages: self.model).bind(onNext: { (image) in
+//                    print(image)
+//            })
+//                observableImages.subscribe { (imagesDictionary) in
+//                    print(imagesDictionary)
+//                }
+
+
+            }) {
+        
+        }
 //        let observableImages = self.rx_getImages()
 //        
 //        observableImages.subscribe(onNext: { (response) in
@@ -73,15 +80,18 @@ class PublicationsPresenter : PublicationsProtocol{
 
     
     func rx_getPublications() -> Observable<[PromotedMessage]> {
-        return Observable.create({ (observe)  in
+        return Observable.create ({ (observe)  in
             let request = Alamofire.request("https://api.voxfeed.com/public/promoted_messages", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON(completionHandler: { (response) in
                 guard response.error == nil else {
                     return observe.onError(response.error!)
                 }
                 let array = response.value as! NSArray
                 self.model = array.map({ (dictionary) -> PromotedMessage in
+                    
                     return PromotedMessage.init(data : dictionary as! NSDictionary)
+                    
                 })
+                observe.onNext(self.model)
                 observe.onCompleted()
             })
         return Disposables.create { request.cancel() }
@@ -126,21 +136,46 @@ class PublicationsPresenter : PublicationsProtocol{
         })
     }
     
-//    func rx_retriveImages(promotedMessages : [PromotedMessage]) -> Observable<UIImage> {
-//        return Observable.create({ (observeImage) -> Disposable in
-//            for promoteMessage in self.model {
-//                var img : UIImage!
-//                self.retriveImage(promotedMessage: promoteMessage, success: { (image) in
-//                    var dictionary = NSDictionary()
-//                        dictionary = ["id" :  promoteMessage.getId(), "image" : image]
-//                            imagesDictionaryArray.append(dictionary)
-//                            if imagesDictionaryArray.count == self.model.count {
-//                                success(imagesDictionaryArray)
-//                            }
-//                        })
+//    func rx_retriveImages(promotedMessages : [PromotedMessage]) -> Observable<[UIImage]> {
+//        var imagesArray : [UIImage]!
+//        let manager = SDWebImageManager()
+//        let observableArray : Observable<[UIImage]>!
+//        var observerdictionary : Observable<NSDictionary>!
+//        var imagesSecuente : Observable<[UIImage]>!
+//      return Observable.create ({ (observeFinish)  in
+//       Observable<PromotedMessage>.from(promotedMessages).map { (promotedMessageItem) -> String  in
+//            return promotedMessageItem.getPost().getImage()
+//            }.map { (urlImage) -> Observable<UIImage> in
+//                return Observable.create ({ (observe)  in
+//                let manager = SDWebImageManager()
+//                manager.imageDownloader?.downloadImage(with: URL.init(string:urlImage), options: SDWebImageDownloaderOptions.continueInBackground, progress: nil,
+//                        completed: { (image, data, error, complete) in
+//                        guard error == nil else {
+//                            return
+//                        }
+//                        return observe.onNext(image!)
+//                })
+//                 return Disposables.create {  }
+//                })
+//        }.bind(onNext: { (observableImage) in
+//                    observableImage.bind(onNext: finalImage)
+//                    imagesArray.append(finalImage)
+//                    if imagesArray.count == promotedMessages.count {
+//                        imagesSecuente = Observable.from(optional: imagesArray)
 //                    }
-//            return Disposables.create {  }
+//                }).dispose()
+//
 //        })
+//
+//        imagesSecuente.subscribe { (eventImage) in
+//            switch eventImage {
+//                case .completed : observeFinish.onNext(imagesArray)
+//                case .error : return
+//            case .next(_):
+//                print("next")
+//            }
+//        } return Disposables.create {  }
+//
 //    }
-    
 }
+
